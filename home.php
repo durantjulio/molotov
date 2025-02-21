84742c404c3acb1bc166c30653f55fbd6339eaf9
durantjulio <juliocesar30708344@gmail.com>
2025-02-20 20:02:13 -0400

    mi primer commit

---
+++ home.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MEWING_RECORDS </title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css"
+        integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Sigmar&display=swap" rel="stylesheet">
+<body>
+    <main>
+        <header>
+            <div class="menu encabezado">
+                <h1>Mewing<strong class="bold">Records</strong></h1>
+                <nav class="navba">
+                    <ul>
+                        <li><a href="home.php" >Home</a></li>
+                        <li><a href="Music.html" >Music</a></li>
+                        <li><a href="Contact.html" >Contact</a></li>
+                        <li><a href="/carrito/Store.html"><i class="fa-solid fa-cart-shopping icon"></i></a></li>
+                        <li><a href="/inicio/index.php"><i class="fa-solid fa-user"></i></a></li>
+                        <li><a href="conexion/logout.php"><i class="fa-solid fa-right-from-bracket"></i></a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        <div class="Portada">
+            <span class="titulos">
+                <h3>MOLOTOV</h3>
+                <h1>Mewing<strong class="bold">Records</h1>
+                
+            </span>
+            <div class="img-yo">
+                <img src="/img/molotov.png"  alt="Mi_imagen">
+            </div>
+        </div>
+    </main>
+    
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css"
+        integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Mewgins records</title>
+    <link rel="stylesheet" href="inicio.css">
+</head>
+
+<body>
+    <section class="navs">
+        <main>
+            <header>
+                <div class="embacezado">
+                    <nav class="navba">
+                        <ul>
+                            <li><i class="fa-brands fa-instagram"></i></li>
+                            <li><i class="fa-brands fa-facebook"></i></li>
+                            <li><i class="fa-brands fa-x-twitter"></i></li>
+                            <li><i class="fa-brands fa-whatsapp"></i></li>
+                            <li><i class="fa-brands fa-tiktok"></i></li>
+                        </ul>
+                    </nav>
+                    <h1><a href="/index.php">Mewing<strong class="bold">Records</strong></a></h1>
+                </div>
+            </header>
+        </main>
+    </section>
+    <section class="formulario">
+        <form action="conexion/validar.php" method="POST">
+            <div class="mb-3">
+                <label for="email_per" class="form-label"><i class="fa-solid fa-envelope"></i></label>
+                <input type="email" name="email" id="email" placeholder="your@gmail.com"
+                    required>
+            </div>
+            <div class="mb-4">
+                <label for="password" class="form-label"><i class="fa-solid fa-lock"></i></label>
+                <input type="text" name="password" id="password" placeholder="xxxxxx" required>
+            </div>
+            <button type="submit" class="btn">Enviar</button>
+            <button type="submit">Crear cuenta</button>
+            <?php
+        if(isset($_GET['error']) && $_GET['error'] == 1){
+            echo "
+            <p class='error'>Error en la autenticación</p>
+            ";
+        }
+        if(isset($_GET['sesion']) && $_GET['sesion'] == 1){
+            echo "
+            <p class='sesion'>Sesion cerrada correctamente</p>
+            ";
+        }
+        ?>
+        </form>
+    </section>
+
+</body>
+
+</html>
